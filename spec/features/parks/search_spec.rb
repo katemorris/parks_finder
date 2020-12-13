3a41df6df15de21572430d42d532c0b89e90eddc
Kate Tester <katemorris@gmail.com>
2020-12-13 15:06:19 -0700

    Adds test for park search

---
+++ spec/features/parks/search_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'When I select a state to find parks there' do
+    it 'I see total parks and a list of parks' do
+      visit root_path
+
+      select 'Tennessee'
+      click_on 'Find Parks'
+
+      expect(page).to have_content('Parks: 16')
+      within('#chch') do
+        expect(page).to have_content('Chickamauga & Chattanooga National Military Park')
+        expect(page).to have_content('In 1863, Union and Confederate forces fought for control of Chattanooga')
+        expect(page).to have_link('Directions')
+        expect(page).to have_content('Sunday: Sunrise to Sunset')
+      end
+
+    end
+  end
+end
