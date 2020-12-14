d21bafa0331d51a5bf6a9282830f0b02d8b964d3
Kate Tester <katemorris@gmail.com>
2020-12-13 17:49:12 -0700

    Adds VCR and webmock

--- spec/features/park_service_spec.rb
+++ spec/features/park_service_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe ParkService, type: :model do
   describe 'class methods' do
     it '.state_parks()' do
-      state = 'TN'
-      results = ParkService.state_parks(state)
-      expect(results).to be_a Hash
-      expect(results[:data].count).to eq(16)
-      expect(results[:data].first[:fullName]).to be_a String
+      VCR.use_cassette('tenn_parks') do
+        state = 'TN'
+        results = ParkService.state_parks(state)
+        expect(results).to be_a Hash
+        expect(results[:data].count).to eq(16)
+        expect(results[:data].first[:fullName]).to be_a String
+      end
     end
   end
 end
--- spec/features/parks_facade_spec.rb
+++ spec/features/parks_facade_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe ParksFacade, type: :model do
   describe 'class methods' do
     it '.search_state' do
-      state = 'TN'
-      results = ParksFacade.search_state(state)
-      expect(results).to be_an Array
-      expect(results.count).to eq(16)
-      expect(results.first).to be_a Park
+      VCR.use_cassette('tenn_parks') do
+        state = 'TN'
+        results = ParksFacade.search_state(state)
+        expect(results).to be_an Array
+        expect(results.count).to eq(16)
+        expect(results.first).to be_a Park
+      end
     end
   end
 end
